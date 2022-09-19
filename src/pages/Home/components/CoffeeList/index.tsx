0760563a17c84bff600d23bd246d08495898a2d2
BrunoVigel <brunorosav@gmail.com>
2022-09-19 16:22:11 -0300

    test fix img src 1

--- src/pages/Home/components/CoffeeList/index.tsx
+++ src/pages/Home/components/CoffeeList/index.tsx
@@ -1,5 +1,5 @@
 import { BuyButtons, BuyContainer, CoffeeListContainer, Item, ItemDescription, ItemList, ItemTags, Title } from "./style";
-import TestImg from '../../../../Assets/image.png'
+// import TestImg from '../../../../Assets/image.png'
 import { Minus, Plus, ShoppingCart } from "phosphor-react";
 
 
@@ -9,112 +9,7 @@ export function CoffeeList() {
             <Title>Nossos cafés</Title>
             <ItemList>
                 <Item>
-                    <img src={TestImg} alt="" />
-                    <ItemTags>
-                        <p>Tradicional</p>
-                    </ItemTags>
-                    <ItemDescription>
-                        <p>Expresso Tradicional</p>
-                        <p>O tradicional café feito com água quente e grãos moídos</p>
-                    </ItemDescription>
-                    <BuyContainer>
-                        <p><span>R$</span>9,90</p>
-                        <BuyButtons>
-                            <div>
-                                <button><Minus color="#8047F8"/></button>
-                                <span>1</span>
-                                <button><Plus color="#8047F8"/></button>
-                            </div>
-                            <button><ShoppingCart size={16} color='#F3F2F2'/></button>
-                        </BuyButtons>
-                    </BuyContainer>
-                </Item>
-                <Item>
-                    <img src={TestImg} alt="" />
-                    <ItemTags>
-                        <p>Tradicional</p>
-                    </ItemTags>
-                    <ItemDescription>
-                        <p>Expresso Tradicional</p>
-                        <p>O tradicional café feito com água quente e grãos moídos</p>
-                    </ItemDescription>
-                    <BuyContainer>
-                        <p><span>R$</span>9,90</p>
-                        <BuyButtons>
-                            <div>
-                                <button><Minus color="#8047F8"/></button>
-                                <span>1</span>
-                                <button><Plus color="#8047F8"/></button>
-                            </div>
-                            <button><ShoppingCart size={16} color='#F3F2F2'/></button>
-                        </BuyButtons>
-                    </BuyContainer>
-                </Item>
-                <Item>
-                    <img src={TestImg} alt="" />
-                    <ItemTags>
-                        <p>Tradicional</p>
-                    </ItemTags>
-                    <ItemDescription>
-                        <p>Expresso Tradicional</p>
-                        <p>O tradicional café feito com água quente e grãos moídos</p>
-                    </ItemDescription>
-                    <BuyContainer>
-                        <p><span>R$</span>9,90</p>
-                        <BuyButtons>
-                            <div>
-                                <button><Minus color="#8047F8"/></button>
-                                <span>1</span>
-                                <button><Plus color="#8047F8"/></button>
-                            </div>
-                            <button><ShoppingCart size={16} color='#F3F2F2'/></button>
-                        </BuyButtons>
-                    </BuyContainer>
-                </Item>
-                <Item>
-                    <img src={TestImg} alt="" />
-                    <ItemTags>
-                        <p>Tradicional</p>
-                    </ItemTags>
-                    <ItemDescription>
-                        <p>Expresso Tradicional</p>
-                        <p>O tradicional café feito com água quente e grãos moídos</p>
-                    </ItemDescription>
-                    <BuyContainer>
-                        <p><span>R$</span>9,90</p>
-                        <BuyButtons>
-                            <div>
-                                <button><Minus color="#8047F8"/></button>
-                                <span>1</span>
-                                <button><Plus color="#8047F8"/></button>
-                            </div>
-                            <button><ShoppingCart size={16} color='#F3F2F2'/></button>
-                        </BuyButtons>
-                    </BuyContainer>
-                </Item>
-                <Item>
-                    <img src={TestImg} alt="" />
-                    <ItemTags>
-                        <p>Tradicional</p>
-                    </ItemTags>
-                    <ItemDescription>
-                        <p>Expresso Tradicional</p>
-                        <p>O tradicional café feito com água quente e grãos moídos</p>
-                    </ItemDescription>
-                    <BuyContainer>
-                        <p><span>R$</span>9,90</p>
-                        <BuyButtons>
-                            <div>
-                                <button><Minus color="#8047F8"/></button>
-                                <span>1</span>
-                                <button><Plus color="#8047F8"/></button>
-                            </div>
-                            <button><ShoppingCart size={16} color='#F3F2F2'/></button>
-                        </BuyButtons>
-                    </BuyContainer>
-                </Item>
-                <Item>
-                    <img src={TestImg} alt="" />
+                    {/* <img src={TestImg} alt="" /> */}
                     <ItemTags>
                         <p>Tradicional</p>
                     </ItemTags>
